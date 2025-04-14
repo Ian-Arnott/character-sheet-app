f83e207d6b6ed86beed19832c512044b5f7237ee
ianitba <iarnott@itba.edu.ar>
2025-04-14 17:52:17 -0300

    fix: hydration issues

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig: NextConfig = {
